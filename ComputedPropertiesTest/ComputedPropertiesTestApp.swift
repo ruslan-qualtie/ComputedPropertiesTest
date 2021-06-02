f6d73b3a215422f0a6c65e7bd1f8bfaf48843777
Ruslan L.Soldatenko <ruslan@qualtie.com>
2021-06-02 15:27:41 +0300

    Initial Commit

---
+++ ComputedPropertiesTest/ComputedPropertiesTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComputedPropertiesTestApp.swift
+//  ComputedPropertiesTest
+//
+//  Created by Administrator on 6/2/21.
+//
+
+import SwiftUI
+
+@main
+struct ComputedPropertiesTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComputedPropertiesTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ComputedPropertiesTest
+//
+//  Created by Administrator on 6/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

7c858357487cd762b598d75c9139cba1f2223a29
Ruslan L.Soldatenko <ruslan@qualtie.com>
2021-07-21 12:29:26 +0300

    added NSApplicationDelegateAdaptor

--- ComputedPropertiesTest/ComputedPropertiesTestApp.swift
+++ ComputedPropertiesTest/ComputedPropertiesTestApp.swift
@@ -2,9 +2,22 @@ import SwiftUI
 
 @main
 struct ComputedPropertiesTestApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var adapter
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject {}
+
+extension AppDelegate: NSApplicationDelegate {
+    public func applicationDidFinishLaunching(_ notification: Notification) {
+            print("applicationDidFinishLaunching")
+        }
+
+    public func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        true
     }
 }
